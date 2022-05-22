25101871c273a2ba2b074b430a73fe822eea127b
erikAWTO <46092285+erikAWTO@users.noreply.github.com>
2022-05-22 23:34:17 +0200

    Add files via upload

--- sketch_may20a.ino
+++ sketch_may20a.ino
@@ -23,13 +23,17 @@ void setup() {
 
   pinMode(9, OUTPUT); 
   pinMode(10, OUTPUT);
+
+  setSpeedLeft(104);
+  setSpeedRight(100);
 }
 
 void loop() {
-  forward(3000,200);
-  turnRight(500,100);
-  backward(2000,100);
-  turnLeft(500,100);
+  delay(1000);
+  forward(2000);
+  turnRight(750);
+  backward(1000);
+  turnLeft(750);
   
   delay(1000);
   sweepRightServo();
@@ -37,12 +41,10 @@ void loop() {
   sweepLeftServo();
 }
 
-void forward(int ms, int spd)
+void forward(int ms)
 {
   delay(500);
-  analogWrite(9, spd);
-  analogWrite(10, spd);
-
+  
   digitalWrite(motor1pin1, HIGH);
   digitalWrite(motor1pin2, LOW);
 
@@ -52,12 +54,9 @@ void forward(int ms, int spd)
   stopCar(500);
 }
 
-void backward(int ms, int spd)
+void backward(int ms)
 {
   delay(500);
-  analogWrite(9, spd);
-  analogWrite(10, spd);
-
   digitalWrite(motor1pin1, LOW);
   digitalWrite(motor1pin2, HIGH);
 
@@ -67,12 +66,36 @@ void backward(int ms, int spd)
   stopCar(500);
 }
 
-void turnLeft(int ms, int spd)
+void leftBank()
 {
   delay(500);
-  analogWrite(9, spd);
-  analogWrite(10, spd);
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, HIGH);
   
+  delay(750); //750 at 100spd
+  stopCar(500);
+}
+
+void rightBank()
+{
+  delay(500);
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, HIGH);
+
+  digitalWrite(motor2pin1, HIGH);
+  digitalWrite(motor2pin2, LOW);
+  
+  delay(750); //750 at 100spd
+  stopCar(500);
+  
+}
+
+void turnLeft(int ms)
+{
+  delay(500);
   digitalWrite(motor1pin1, HIGH);
   digitalWrite(motor1pin2, LOW);
 
@@ -84,12 +107,9 @@ void turnLeft(int ms, int spd)
   
 }
 
-void turnRight(int ms, int spd)
+void turnRight(int ms)
 {
   delay(500);
-  analogWrite(9, spd);
-  analogWrite(10, spd);
-  
   digitalWrite(motor1pin1, LOW);
   digitalWrite(motor1pin2, HIGH);
 
@@ -137,4 +157,66 @@ void sweepLeftServo()
     leftArm.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
+}
+
+void setPosRightServo(int setPos)
+{
+  int pos = rightArm.read();
+
+  if(setPos <= pos)
+  {
+    for (pos; pos <= setPos; pos += 10) 
+    { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    leftArm.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);
+    }                   // waits 15ms for the servo to reach the position
+  }
+  else
+  {
+    for (pos; pos >= setPos; pos -= 10) 
+    { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    leftArm.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15); 
+    }// waits 15ms for the servo to reach the position
+  }
+}
+
+void setPosLeftServo(int setPos)
+{
+  int pos = leftArm.read();
+
+  if(setPos <= pos)
+  {
+    for (pos; pos <= setPos; pos += 10) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    leftArm.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);      
+    }// waits 15ms for the servo to reach the position
+  }
+  else
+  {
+    for (pos; pos >= setPos; pos -= 10) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    leftArm.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);       
+    }// waits 15ms for the servo to reach the position
+  }
+}
+
+enum motor
+{
+  left,
+  right
+};
+
+void setSpeedLeft(int spd)
+{
+   analogWrite(10, spd);
+}
+
+void setSpeedRight(int spd)
+{
+    analogWrite(9, spd);
 }
