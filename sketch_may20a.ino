097beba11c6fa01e21e6af40362789c0e6dffaac
erikAWTO <46092285+erikAWTO@users.noreply.github.com>
2022-05-31 21:18:18 +0200

    Update sketch_may20a.ino

--- sketch_may20a.ino
+++ sketch_may20a.ino
@@ -30,15 +30,13 @@ void setup() {
 
 void loop() {
   delay(1000);
+  
   forward(2000);
   turnRight(750);
   backward(1000);
   turnLeft(750);
   
-  delay(1000);
-  sweepRightServo();
-  delay(1000);
-  sweepLeftServo();
+  //leftArm.write(2000);
 }
 
 void forward(int ms)
@@ -64,33 +62,6 @@ void backward(int ms)
   digitalWrite(motor2pin2, HIGH);
   delay(ms);
   stopCar(500);
-}
-
-void leftBank()
-{
-  delay(500);
-  digitalWrite(motor1pin1, HIGH);
-  digitalWrite(motor1pin2, LOW);
-
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, HIGH);
-  
-  delay(750); //750 at 100spd
-  stopCar(500);
-}
-
-void rightBank()
-{
-  delay(500);
-  digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, HIGH);
-
-  digitalWrite(motor2pin1, HIGH);
-  digitalWrite(motor2pin2, LOW);
-  
-  delay(750); //750 at 100spd
-  stopCar(500);
-  
 }
 
 void turnLeft(int ms)
@@ -130,86 +101,6 @@ void stopCar(int ms)
 
   delay(ms);
 }
-
-void sweepRightServo()
-{
-  for (int pos = 0; pos <= 1500; pos += 10) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    rightArm.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (int pos = 1500; pos >= 0; pos -= 10) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    rightArm.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-}
-
-void sweepLeftServo()
-{
-  for (int pos = 0; pos <= 1500; pos += 10) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    leftArm.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (int pos = 1500; pos >= 0; pos -= 10) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    leftArm.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-}
-
-void setPosRightServo(int setPos)
-{
-  int pos = rightArm.read();
-
-  if(setPos <= pos)
-  {
-    for (pos; pos <= setPos; pos += 10) 
-    { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    leftArm.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);
-    }                   // waits 15ms for the servo to reach the position
-  }
-  else
-  {
-    for (pos; pos >= setPos; pos -= 10) 
-    { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    leftArm.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15); 
-    }// waits 15ms for the servo to reach the position
-  }
-}
-
-void setPosLeftServo(int setPos)
-{
-  int pos = leftArm.read();
-
-  if(setPos <= pos)
-  {
-    for (pos; pos <= setPos; pos += 10) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    leftArm.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);      
-    }// waits 15ms for the servo to reach the position
-  }
-  else
-  {
-    for (pos; pos >= setPos; pos -= 10) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    leftArm.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);       
-    }// waits 15ms for the servo to reach the position
-  }
-}
-
-enum motor
-{
-  left,
-  right
-};
 
 void setSpeedLeft(int spd)
 {
